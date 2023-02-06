293ddf7afbffa8808d35be151706f90320942d5c
Damian Velluppillai <dvelluppillai@gmail.com>
2023-02-06 14:38:39 +1300

    Incremental update

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:airport/airports.dart';
@@ -14,66 +13,84 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+// Initialise Google Earth Map widget properties
+
   Completer<GoogleMapController> _controller = Completer();
 
-  static const LatLng _center = const LatLng(-37, 173);
-
-  static final resultado = Airports.getAirport("SBSP");
+  static const LatLng _center = const LatLng(-40, 173);
 
   void _onMapCreated(GoogleMapController controller) {
     _controller.complete(controller);
   }
+
+  final FromAirportInputController = TextEditingController();
+  final ToAirportInputController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Flight track calculator'),
+          title: const Text('Flight track calculator'),
           backgroundColor: Colors.green[700],
         ),
         body: Stack(
           children: <Widget>[
             GoogleMap(
               onMapCreated: _onMapCreated,
-              initialCameraPosition: CameraPosition(
+              initialCameraPosition: const CameraPosition(
                 target: _center,
                 zoom: 5.0,
               ),
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                new Flexible(
-                  child: Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: new TextField(
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        contentPadding: EdgeInsets.all(10),
-                        filled: true,
-                        fillColor: Colors.white,
-                        labelText: 'From Airport',
+            Container(
+              color: Colors.white,
+              width: double.infinity,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Flexible(
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: TextField(
+                        controller: FromAirportInputController,
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          contentPadding: EdgeInsets.all(10),
+                          filled: true,
+                          fillColor: Colors.white,
+                          labelText: 'From Airport',
+                        ),
                       ),
                     ),
                   ),
-                ),
-                new Flexible(
-                  child: Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: new TextField(
-                      decoration: InputDecoration(
-                        filled: true,
-                        fillColor: Colors.white,
-                        border: OutlineInputBorder(),
-                        contentPadding: EdgeInsets.all(10),
-                        labelText: 'To Airport',
+                  Flexible(
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: TextField(
+                        controller: ToAirportInputController,
+                        decoration: const InputDecoration(
+                          filled: true,
+                          fillColor: Colors.white,
+                          border: OutlineInputBorder(),
+                          contentPadding: EdgeInsets.all(10),
+                          labelText: 'To Airport',
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ],
+                  Flexible(
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        child: const Text('Calculate'),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             )
           ],
         ),
